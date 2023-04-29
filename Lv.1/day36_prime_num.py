eeef5a21ba26caed4c1e3a8effe525515455cc12
lalaang <kraeyong@gmail.com>
2023-04-29 22:14:06 +0900

    20230427

--- Lv.1/day36_prime_num.py
+++ Lv.1/day36_prime_num.py
@@ -12,3 +12,13 @@ def solution(n):
         if prime_number(i):
             answer += 1
     return answer
+
+# 에라토스테네스의 체 사용하는 방법
+
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
