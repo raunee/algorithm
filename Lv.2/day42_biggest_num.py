40052332c0e6730b5c3d5acc02845168f6698499
lalaang <kraeyong@gmail.com>
2023-05-06 23:12:14 +0900

    20230504

---
+++ Lv.2/day42_biggest_num.py
@@ -0,0 +1,6 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42746
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x : x*3, reverse=True)
+    return str(int(''.join(numbers)))
